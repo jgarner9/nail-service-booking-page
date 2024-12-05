5b3b38030c2f82f4a4b276d71e64c1e2e5389695
Jason <garnerjasonj@icloud.com>
2024-12-05 09:10:45 +0000

    create ConfirmationPage component

--- src/components/ConfirmationPage.jsx
+++ src/components/ConfirmationPage.jsx
@@ -1,7 +1,28 @@
+import { useContext } from "react";
+import useCustomer from "../hooks/useCustomer";
+import { context } from "./ContextProvider";
+
 export default function ConfirmationPage() {
+  const { selectedService, selectedDate, selectedTime, customerInfo } =
+    useContext(context);
+
+  useCustomer();
   return (
     <div id="confirmation-page-wrapper">
       <h1 className="title">Confirmation</h1>
+      <h2>
+        <bold>Service:</bold> {selectedService}
+      </h2>
+      <h2>
+        <bold>Date:</bold> {selectedDate.format("MM-DD-YYYY")}
+      </h2>
+      <h2>
+        <bold>Time:</bold> {selectedTime}
+      </h2>
+      <h2>
+        <bold>Customer Info:</bold> {customerInfo.firstName}{" "}
+        {customerInfo.lastName}
+      </h2>
     </div>
   );
 }
