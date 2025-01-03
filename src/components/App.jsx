5b6974f3059be87f802161b1424faa21b3eba936
Jason <garnerjasonj@icloud.com>
2025-01-03 08:56:01 +0000

    move ThemeProvider component to main.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,12 +3,7 @@ import Header from "./Header";
 import StepTracker from "./StepTracker";
 import ServicePage from "./ServicePage";
 import useServices from "../hooks/useServices";
-import {
-  Button,
-  CircularProgress,
-  createTheme,
-  ThemeProvider,
-} from "@mui/material";
+import { Button, CircularProgress } from "@mui/material";
 import AppointmentDatePage from "./AppointmentDatePage";
 import CustomerInfoPage from "./CustomerInfoPage";
 import ConfirmationPage from "./ConfirmationPage";
@@ -31,115 +26,99 @@ export default function App() {
     setError,
   } = useContext(context);
   const services = useServices();
-  const theme = createTheme({
-    palette: {
-      primary: {
-        main: "#1c2e20",
-      },
-      secondary: {
-        main: "#2c4e34",
-      },
-      contrastText: "#f7e7d6",
-    },
-  });
 
   return (
-    <ThemeProvider theme={theme}>
-      <div id="app-wrapper">
-        <Header>
-          <StepTracker currentStep={currentStep}></StepTracker>
-        </Header>
-        <div id="page-wrapper">
-          {currentStep === 1 && <ServicePage services={services} />}
-          {currentStep === 2 && <AppointmentDatePage />}
-          {currentStep === 3 && <CustomerInfoPage />}
-          {currentStep === 4 && <ConfirmationPage services={services} />}
-          {isLoading && currentStep === 5 && (
-            <>
-              <h2>Request Processing</h2>
-              <br />
-              <CircularProgress />
-            </>
+    <div id="app-wrapper">
+      <Header>
+        <StepTracker currentStep={currentStep}></StepTracker>
+      </Header>
+      <div id="page-wrapper">
+        {currentStep === 1 && <ServicePage services={services} />}
+        {currentStep === 2 && <AppointmentDatePage />}
+        {currentStep === 3 && <CustomerInfoPage />}
+        {currentStep === 4 && <ConfirmationPage services={services} />}
+        {isLoading && currentStep === 5 && (
+          <>
+            <h2>Request Processing</h2>
+            <br />
+            <CircularProgress />
+          </>
+        )}
+        {!isLoading && currentStep === 5 && (
+          <>
+            <h1>Appointment Booked!</h1>
+            <h2>
+              Service:{" "}
+              {services.find((service) => service.id === selectedService).name}
+            </h2>
+            <h2>Date: {selectedDate.format("MM-DD-YYYY")}</h2>
+            <h2>Time: {formatTime(selectedTime)}</h2>
+            <h2>
+              Customer Info: {customerInfo.firstName} {customerInfo.lastName}
+            </h2>
+          </>
+        )}
+        <div id="nav-wrapper">
+          {currentStep > 1 && currentStep <= 4 && (
+            <Button
+              id="back-button"
+              variant="outlined"
+              onClick={() => {
+                setError(null);
+                setCurrentStep(currentStep - 1);
+              }}
+            >
+              Back
+            </Button>
           )}
-          {!isLoading && currentStep === 5 && (
-            <>
-              <h1>Appointment Booked!</h1>
-              <h2>
-                Service:{" "}
-                {
+          {currentStep < 4 && (
+            <Button
+              id="next-button"
+              variant="contained"
+              onClick={() =>
+                validateStepChange(
+                  currentStep,
+                  setCurrentStep,
+                  selectedService,
+                  selectedDate,
+                  selectedTime,
+                  customerInfo,
+                  setError
+                )
+              }
+            >
+              Next
+            </Button>
+          )}
+          {currentStep === 4 && (
+            <Button
+              variant="contained"
+              id="submit-button"
+              onClick={() => {
+                console.log(customerInfo.notes);
+                submitAppointment(
+                  selectedService,
+                  customerInfo.id,
+                  dayjs(selectedDate).format("MM-DD-YYYY"),
+                  selectedTime,
                   services.find((service) => service.id === selectedService)
-                    .name
-                }
-              </h2>
-              <h2>Date: {selectedDate.format("MM-DD-YYYY")}</h2>
-              <h2>Time: {formatTime(selectedTime)}</h2>
-              <h2>
-                Customer Info: {customerInfo.firstName} {customerInfo.lastName}
-              </h2>
-            </>
-          )}
-          <div id="nav-wrapper">
-            {currentStep > 1 && currentStep <= 4 && (
-              <Button
-                id="back-button"
-                variant="outlined"
-                onClick={() => {
-                  setError(null);
-                  setCurrentStep(currentStep - 1);
-                }}
-              >
-                Back
-              </Button>
-            )}
-            {currentStep < 4 && (
-              <Button
-                id="next-button"
-                variant="contained"
-                onClick={() =>
-                  validateStepChange(
-                    currentStep,
-                    setCurrentStep,
-                    selectedService,
-                    selectedDate,
-                    selectedTime,
-                    customerInfo,
-                    setError
-                  )
-                }
-              >
-                Next
-              </Button>
-            )}
-            {currentStep === 4 && (
-              <Button
-                variant="contained"
-                id="submit-button"
-                onClick={() => {
-                  console.log(customerInfo.notes);
-                  submitAppointment(
-                    selectedService,
-                    customerInfo.id,
-                    dayjs(selectedDate).format("MM-DD-YYYY"),
-                    selectedTime,
-                    services.find((service) => service.id === selectedService)
-                      .duration,
-                    customerInfo.notes,
-                    setIsLoading
-                  );
-                  setCurrentStep(5);
-                }}
-              >
-                Submit
-              </Button>
-            )}
-          </div>
-          {error && (
-            <div id="error-message-wrapper">
-              <h2 id="error-message">{error}</h2>
-            </div>
+                    .duration,
+                  customerInfo.notes,
+                  setIsLoading
+                );
+                setCurrentStep(5);
+              }}
+            >
+              Submit
+            </Button>
           )}
         </div>
+        {error && (
+          <div id="error-message-wrapper">
+            <h2 id="error-message">{error}</h2>
+          </div>
+        )}
       </div>
-    </ThemeProvider>
+    </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,25 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./components/App";
 import ContextProvider from "./components/ContextProvider";
+import { ThemeProvider } from "@emotion/react";
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#1c2e20",
+    },
+    secondary: {
+      main: "#2c4e34",
+    },
+    contrastText: "#f7e7d6",
+  },
+});
 
 createRoot(document.getElementById("root")).render(
-  <ContextProvider>
-    <App />
-  </ContextProvider>
+  <ThemeProvider theme={theme}>
+    <ContextProvider>
+      <App />
+    </ContextProvider>
+  </ThemeProvider>
 );
